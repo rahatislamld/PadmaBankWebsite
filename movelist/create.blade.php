05c243161835231c366e3ec9ac364c2a6b729256
rahatislamld <48786977+rahatislamld@users.noreply.github.com>
2023-04-13 15:00:23 +0600

    Add files via upload

--- movelist/create.blade.php
+++ movelist/create.blade.php
@@ -92,104 +92,27 @@
                     </div>
                     <div class="x_content">
 
-                        <form class="form-horizontal form-label-left"   action="{{ route('movelist.store') }}"  method="POST" enctype="multipart/form-data">
-                            @csrf
-                            <div class="form-group vinput_main_branch">
-                                <label  class="control-label col-md-3 col-sm-3 col-xs-12">
-                                    Name
-                                </label>
-                                <div class="col-md-6 col-sm-6 col-xs-12">
-                                    <input type="text" class="form-control"
-                                name="name" aria-label="First name">
-                                </div>
+                        <div class="OneOfTwo" style="width:60%">
+                            <div style="float:left;">
+                            <strong>Select 1.</strong><br />
+                            <select id="available" size="5" multiple="multiple" style="width: 270px;" class="select2">
+                            <option>item1</option>
+                            <option>item2</option>
+                            <option>item3</option>
+                            <option>item4</option>
+                            <option>item5</option>
+                            </select>
                             </div>
-
-                            <div class="form-group vinput_main_branch">
-                                <label for="parent branch" class="control-label col-md-3 col-sm-3 col-xs-12">
-                                    Designation
-                                </label>
-                                <div class="col-md-6 col-sm-6 col-xs-12">
-                                    <select class="form-control select2" name="designation">
-                                        
-                                     
-                                            <option value="1" selected >
-                                              MD
-                                            </option>
-                                            <option value="2" >
-                                                DMD
-                                              </option>
-                                    
-                                  
-                                    </select>
-                                </div>
+                            <div style="float:left;height:50px;padding-top:30px;">
+                            <input type="button" class="testButton" value="<" onclick="swapElement('selected','available')" /><br />
+                            <input type="button" class="testButton" value=">" onclick="swapElement('available','selected')" style="padding-left:12px;" />
                             </div>
-                            <div class="form-group vinput_main_branch">
-                                <label  class="control-label col-md-3 col-sm-3 col-xs-12">
-                                    Email
-                                </label>
-                                <div class="col-md-6 col-sm-6 col-xs-12">
-                                    <input type="email" class="form-control"
-                                name="email" aria-label="email">
-                                </div>
+                            <div style="float:left;">
+                            <strong>Select 2.</strong><br />
+                            <select id="selected" size="5" multiple="multiple" style="width: 170px;">
+                            </select>
                             </div>
-                            <div class="form-group vinput_main_branch">
-                                <label  class="control-label col-md-3 col-sm-3 col-xs-12">
-                                    Phone
-                                </label>
-                                <div class="col-md-6 col-sm-6 col-xs-12">
-                                    <input type="number" class="form-control"
-                                name="phone" aria-label="phone">
-                                </div>
                             </div>
-                            <div class="form-group vinput_main_branch">
-                                <label  class="control-label col-md-3 col-sm-3 col-xs-12">
-                                    Image
-                                </label>
-                                <div class="col-md-6 col-sm-6 col-xs-12">
-                                    <input type="file" class="form-control"
-                                name="image" aria-label="image">
-                                </div>
-                            </div>
-                            <br>
-                            <br>
-                            
-                            
-
-                            <div class="form-group vinput_main_branch">
-                                <label  class="control-label col-md-3 col-sm-3 col-xs-12">
-                                    Image
-                                </label>
-                               
-                            </div>
-
-
-                            
-
-                           
-
-                      <div class="form-group vinput_main_branch">
-                      <div style="text-align: center; margin:10px">
-
-                        <label for="aoi"></label>  
-
-                        <select multiple id="select1" name="aoi[]">  
-                          @foreach ($branches as $cntrl)
-                                            <option value="{{ $cntrl->id }}">
-                                                {{ $cntrl->name }}
-                                            </option>
-                                        @endforeach  
-                        </select> 
-                        <a href="#" id="add">add &gt;&gt;</a>  
-                       </div>  
-                       <div style="text-align: center; margin:10px">  
-                        <select multiple id="select2" name="selected[]"></select>  
-                        <a href="#" id="remove">&lt;&lt; remove</a>  
-                       </div>
-                      </div>
-                      <div class="control-label col-md-3 col-sm-3 col-xs-12">
-                        <button  type="submit" class="btn btn-danger">Submit</button>
-                        </div>
-                        </form>
 
                     </div>
                 </div>
@@ -217,17 +140,19 @@
         $('.select2').select2();
     </script>
 
-<script src="js/jquery.js" type="text/javascript"></script>  
-<script type="text/javascript">  
- $().ready(function() {  
-  $('#add').click(function() {  
-   return !$('#select1 option:selected').remove().appendTo('#select2');  
-  });  
-  $('#remove').click(function() {  
-   return !$('#select2 option:selected').remove().appendTo('#select1');  
-  });  
- });  
-</script>  
+<script>
+    function swapElement(fromList,toList){
+        var selectOptions = document.getElementById(fromList);
+        for (var i = 0; i < selectOptions.length; i++) {
+            var opt = selectOptions[i];
+            if (opt.selected) {
+                document.getElementById(fromList).removeChild(opt);
+                document.getElementById(toList).appendChild(opt);
+                i--;
+            }
+        }
+    }
+    </script> 
     {{-- <script src="https://code.jquery.com/jquery-3.6.3.min.js"
         integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script> --}}
 
